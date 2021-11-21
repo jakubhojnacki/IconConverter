4a6e7a8c98b5d4424d5da7c876058cc0f067fe04
Jakub Hojnacki <jakubhojnacki@gmail.com>
2021-11-21 18:41:00 +0000

    Scripts updated, not working yet

--- run.sh
+++ run.sh
@@ -2,10 +2,9 @@
 
 tty -s || exec konsole -e "$0" "$@"
 
+sourcedirectoryPath="/home/Multimedia/Icons (ICO)"
+destinationDirectoryPath="/home/Temp"
 
-sourcedirectoryPath=/home/Multimedia/Icons (ICO)
-destinationDirectoryPath=/home/Temp
-
-node "./src/main.mjs" "$sourcedirectoryPath" "$destinationDirectoryPath"
+node "./src/main.mjs" $sourcedirectoryPath $destinationDirectoryPath
 
 read -n 1 -s -r -p "(Press any key to end)"
